544ffa42937290af2ff62b5353ab68a7fd33a233
smileywrld <ismaileyyunusa@gmail.com>
2026-05-15 00:18:40 +0100

    feat: set basename for HashRouter to support environment-specific routing

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>
+    <HashRouter basename={import.meta.env.BASE_URL}>
       <App />
     </HashRouter>
   </React.StrictMode>,
